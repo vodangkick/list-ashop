cde87030e7e511e477ade4470d307d7ce2344b37
vodangkick <quang115thd@gmail.com>
2020-07-14 05:40:00 -0700

    test bewqe

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import Shop from './pages/Shop';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
-import Cart from './pages/Cart';
-import CheckOut from './pages/CheckOut';
+// import { Switch, Route } from 'react-router-dom';
+// import Home from "./pages/Home";
+// import Shop from './pages/Shop';
+// import Blog from './pages/Blog';
+// import Contact from './pages/Contact';
+// import Cart from './pages/Cart';
+// import CheckOut from './pages/CheckOut';
 
-import ProductDetail from './pages/ProductDetail';
-import ScrollToTop from './components/ScrollToTop';
+// import ProductDetail from './pages/ProductDetail';
+// import ScrollToTop from './components/ScrollToTop';
 
 
 function App() {
   return (
-    <>
-      <ScrollToTop />
+    <>]
+    test
+      {/* <ScrollToTop />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/shop' component={Shop} />
@@ -24,7 +25,7 @@ function App() {
         <Route path='/cart' component={Cart} />
         <Route path='/:slug' component={ProductDetail} />
         <Route path='/checkout' component={CheckOut} />
-      </Switch>
+      </Switch> */}
     </>
   );
 }
