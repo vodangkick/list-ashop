1a892d35350e3410b51745d5c19f4a0a3556e609
vodangkick <quang115thd@gmail.com>
2020-07-15 07:25:31 -0700

    upload

--- src/components/sections/FeatureProduct.js
+++ src/components/sections/FeatureProduct.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HotDeal from '../products/HotDeal';
+import 'jquery';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
